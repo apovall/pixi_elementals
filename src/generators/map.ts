606d601ad2b8b5122abe27eeb2e9aa082d27b718
AlanP <alan.g.povall.2@gmail.com>
2022-12-22 21:04:31 +1300

    Add cellular automaton (WIP)

--- src/generators/map.ts
+++ src/generators/map.ts
@@ -7,52 +7,109 @@
 
 export class MapGenerator{
 
-  private readonly width: number
-  private readonly height: number
+  private readonly screenWidth: number
+  private readonly screenHeight: number
+  private readonly floorTile: string
+  private readonly wallTile: string
+  private readonly wallCountThreshold: number
 
   // constructor( height, width)
-  constructor(width:number, height:number,){
-    this.height = height
-    this.width = width
+  constructor(screenWidth:number, screenHeight:number,){
+    this.screenHeight = screenHeight
+    this.screenWidth = screenWidth
+    this.floorTile = '.'
+    this.wallTile = 'x'
+    this.wallCountThreshold = 4
   }
 
-  seedNoiseMap(buildableDensity:number): Array<Array<Boolean>>{
+  seedNoiseMap(buildableDensity:number): Array<Array<String>>{
     /*   
       args:
-        buildableDensity: specifies whether an element will be 'buildable' or not
+        buildableDensity: specifies whether an element will be 'buildable' or not. Treat as percentage.
       returns:
         noiseMap to act as a seed for cellular automaton
         for creating the initial map on which to build 
     */
 
     // Create 2D array[height[row]]
-    let noiseMap:Array<Array<Boolean>> = []
+    let noiseMap:Array<Array<String>> = []
     // Cycle through the array to create a random noise map
-    for(let height:number=0; height<this.height; height++){
+    for(let height=0; height<this.screenHeight; height++){
       noiseMap.push([])
-      for(let width:number=0; width<this.width; width++){
-        if(Math.random() >= buildableDensity){
-          noiseMap[height].push(true)
+      for(let width=0; width<this.screenWidth; width++){
+        if(Math.random() <= buildableDensity){
+          noiseMap[height].push(this.floorTile) //Floor tile
         }
         else{
-          noiseMap[height].push(false)
+          noiseMap[height].push(this.wallTile) //Wall tile
         }
       }
     }
     return noiseMap
   }
   // cellular_automata procedural smoothing (seedMap, passes): map
-  // growth_check(map): newMap
-
-  applyCellularAutomaton(noiseMap:Array<Array<Boolean>>, count:number): Array<Array<Boolean>>{
-    for(let runCount:number=0; runCount < count; runCount++){
-      let tempGrid = noiseMap
-      // For each height array
-        // For each width (row) element
-          // 
-    }    
-    return noiseMap
+  applyCellularAutomaton(grid:Array<Array<String>>, count:number): Array<Array<String>>{
+    /*
+      args:
+        grid - the incoming noise map
+        count - number of times to loop through the cellular automaton algorithm
+      returns:
+        smoothed map for populating
+    */
+    console.log(grid);
+    
+    for(let runCount=0; runCount < count; runCount++){
+      console.log('#1')
+      console.log(grid)
+      for(let passes=1; passes<=count; passes++){
+        let tempGrid = grid // To not work use noiseMap as both input and output
+        for(let heightSearch=1; heightSearch<this.screenHeight; heightSearch++){
+          for(let widthSearch=1; widthSearch<this.screenWidth; widthSearch++){
+            let neighbourWallCount:number = 0
+            for(let heightBounds = heightSearch - 1; heightBounds<=heightSearch+1; heightBounds++ ){
+              for(let widthBounds = widthSearch - 1; widthBounds<=widthSearch+1; widthBounds++){
+                if(this.checkMapBoundary(heightBounds,widthBounds)){
+                  // Check if search item is not equal to the current cell
+                  if(heightBounds != heightSearch || widthBounds != widthSearch){
+                      if(tempGrid[heightBounds][widthBounds] == this.wallTile){
+                        neighbourWallCount++                    
+                      }
+                  }
+                }else{
+                  neighbourWallCount++
+                }
+              }
+            }
+            if(neighbourWallCount > this.wallCountThreshold){
+              grid[heightSearch][heightSearch] = this.wallTile
+            }else{
+              grid[heightSearch][heightSearch] = this.floorTile
+            }
+          }
+        }
+      }
+    }  
+    console.log('#2');
+    console.log(grid);
+      
+    return grid
   }
+
+  checkMapBoundary(searchBounds:number, widthBounds:number):Boolean{
+    try {
+      if(searchBounds <= -1 || searchBounds > this.screenHeight){
+        return false
+      }
+      if(widthBounds <=-1 || widthBounds > this.screenWidth){
+        return false
+      }
+      return true    
+    } catch (error) {
+      return false
+    }
+  }
+
+  // growth_check(map): newMap
 
 
   
